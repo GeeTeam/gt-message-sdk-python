7f38572e84205f945cfd4d47965bda8cd8ed785c
zheng <dreamzsm@gmail.com>
2015-06-16 16:33:50 +0800

    remove the repeat code segment

--- tornado_demo/app.py
+++ tornado_demo/app.py
@@ -6,7 +6,6 @@ import tornado.gen
 import sdk.geetestmsg as geetestmsg
 
 
-BASE_URL = "api.geetest.com/get.php?gt="
 
 captcha_id = "a40fd3b0d712165c5d13e6f747e948d4"
 private_key = "0f1a37e33c9ed10dd2e133fe2ae9c459"
@@ -33,9 +32,6 @@ class ValidateHandler(tornado.web.RequestHandler):
     def post(self, *args, **kwargs):
         try:
             phone = self.get_argument("phone")
-            challenge = self.get_argument("geetest_challenge")
-            validate = self.get_argument("geetest_validate")
-            seccode = self.get_argument("geetest_seccode")
             code = self.get_argument("code")
             gt = geetestmsg.geetest(captcha_id, private_key)
             res = gt.msg_validate(phone=phone, code=code)
