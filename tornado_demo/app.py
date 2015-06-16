5e9167d176b9249b2cd8b3f6f3478e5c81a68f3b
王沛丰 <wangpeifeng@wangpeifengdeMacBook-Pro.local>
2015-06-16 15:11:12 +0800

    添加短信验证码判断

--- tornado_demo/app.py
+++ tornado_demo/app.py
@@ -17,6 +17,8 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
         self.render("static/login.html")
 
+
+class SendHandler(tornado.web.RequestHandler):
     def post(self, *args, **kwargs):
         phone = self.get_argument("phone")
         challenge = self.get_argument("geetest_challenge")
@@ -27,11 +29,32 @@ class MainHandler(tornado.web.RequestHandler):
         self.write(str(res))
 
 
+class ValidateHandler(tornado.web.RequestHandler):
+    def post(self, *args, **kwargs):
+        try:
+            phone = self.get_argument("phone")
+            challenge = self.get_argument("geetest_challenge")
+            validate = self.get_argument("geetest_validate")
+            seccode = self.get_argument("geetest_seccode")
+            code = self.get_argument("code")
+            gt = geetestmsg.geetest(captcha_id, private_key)
+            res = gt.msg_validate(phone=phone, code=code)
+            if res.get("res", 0) == 1:
+                self.write("success")
+                return
+            self.write("fail")
+            return
+        except Exception,e:
+            self.write("fail")    
+
+
 if __name__ == "__main__":
     settings = {
         "static_path": os.path.join(os.path.dirname(__file__), "static")
     }
     app = tornado.web.Application([
+                                      (r"/send", SendHandler),  
+                                      (r"/validate", ValidateHandler),  
                                       (r"(.+)", MainHandler),
                                   ], debug=True, **settings)
 
